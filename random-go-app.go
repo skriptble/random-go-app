b3562479357b66b5a69dfac74fb7bd0588a7c5e7
Kris Brandow <kris.brandow@gmail.com>
2014-03-23 23:49:31 -0400

    Testing out some concurrency stuff, doing as much from memory as I can.

--- random-go-app.go
+++ random-go-app.go
@@ -2,9 +2,23 @@ package main
 
 import (
     "fmt"
+    "strconv"
+    "time"
 )
 
+func doStuff(c chan string) {
+    mega_awesome := <- c
+    fmt.Println("Mega Awesome: " + mega_awesome)
+}
 func main() {
+    c := make(chan string)
+    for i := 0; i < 100; i++ {
+        go doStuff(c)
+    }
+    for j := 0; j < 100; j++ {
+        c <- "whoa " + strconv.Itoa(j)
+        time.Sleep(100 * time.Millisecond)
+    }
     fmt.Println("Hello World! I AM AWESOME")
     fmt.Println("More awesome than you know!")
 }
